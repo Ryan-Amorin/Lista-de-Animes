d7f16b891e5ea5555ffcbb86c413a005daec40ed
Ryan Amorin <ryanamorin45@gmail.com>
2026-05-25 08:12:49 -0300

    adiciona função de mostrar anime pesquisado e valida a pesquisa pelo titulo

--- API.js
+++ API.js
@@ -1,14 +1,35 @@
-'use strict';
+'use strict'; // modo estrito para tornar erros mais evidentes
 
-const limparTela = () => {
+//limpa a tela para moostrar os dados da pesquisa
+const limparTela = () => 
+{
     document.getElementById('titulo').value = '';
 };
 
+//mostra o anime pesquisado
 const mostrarAnimes = (animes) => 
 {
     const lista = document.getElementById('lista-animes');
 
+    lista.innerHTML = ''; 
+    animes.forEach(anime => 
+    {
+        const li = document.createElement('li'); //cria a lista de animes
+        li.textContent = anime.title; //adiciona o titulo do anime
+        lista.appendChild(li); // envia para o HTML
+    });
 }
+
+//validar nome do anime
+const validarNome = (titulo) => 
+{
+    if (!titulo)
+    {
+        alert('Por favor, digite o nome de um anime.');
+        return false;
+    }
+    return true;
+};
 
 // URL base da API Jikan v4
 //procura o anime na URL
@@ -16,4 +37,5 @@ const pesquisarAnime = async (titulo) =>
 {
     
 }
+
 const BASE_URL = 'https://api.jikan.moe/v4/anime';
